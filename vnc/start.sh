7b5e0813911eb83a866ad7825b3960a7ad8293b3
Will Walker <wwalker@udel.edu>
2019-01-10 13:03:05 -0500

    add vnc

---
+++ vnc/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+iptables -A INPUT -i resin-vpn -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -i resin-vpn -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j REJECT
+
+# Rename the files to allow for default connection html page
+ln -s /noVNC/vnc.html /noVNC/index.html
+
+# Start noVNC
+/noVNC/utils/launch.sh --vnc 127.0.0.1:5900 --listen 80
