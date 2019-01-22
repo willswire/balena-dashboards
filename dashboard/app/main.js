bcd89ead542d7234c0acb54d7d0d2edb8151e804
Will Walker <willwalker@roaming-156-35.host.udel.edu>
2019-01-22 12:13:56 -0500

    stuff

--- dashboard/app/main.js
+++ dashboard/app/main.js
@@ -1,21 +1,17 @@
+// Declare constants required for ElectronJS to run properly
 const electron = require('electron');
-
 const {
   app,
   BrowserWindow,
 } = electron;
 
 let mainWindow;
-var slide = 0;
+const TIMEOUT = process.env.TIMEOUT;
+const SLIDE_URLS = process.env.SLIDE_URLS;
+const SLIDE_COUNT = SLIDE_URLS.length;
+var currentSlide = 0;
 
-const electronConfig = {
-  DASHBOARD_ONE: process.env.DASHBOARD_ONE,
-  DASHBOARD_TWO: process.env.DASHBOARD_TWO,
-  DASHBOARD_THREE: process.env.DASHBOARD_THREE,
-  DASHBOARD_FOUR: process.env.DASHBOARD_FOUR,
-  DASHBOARD_TIMEOUT: process.env.DASHBOARD_TIMEOUT
-};
-
+// Main Function
 app.on('ready', () => {
 
   mainWindow = new BrowserWindow({
@@ -37,26 +33,12 @@ app.on('ready', () => {
     console.log(err);
   });
 
-  /*
   // This code is commented out for future additional dashboard slides.
   // Working on the bus map first
   setInterval(function() {
-    switch (slide % 4) {
-      case 0:
-        mainWindow.loadURL(electronConfig.DASHBOARD_ONE);
-        break;
-      case 1:
-        mainWindow.loadURL(electronConfig.DASHBOARD_TWO);
-        break;
-      case 2:
-        mainWindow.loadURL(electronConfig.DASHBOARD_THREE);
-        break;
-      case 3:
-        mainWindow.loadURL(electronConfig.DASHBOARD_FOUR);
-    };
-    slide++;
-  }, electronConfig.DASHBOARD_TIMEOUT);
-  */
+    mainWindow.loadURL(SLIDE_URLS[currentSlide % SLIDE_COUNT]);
+    currentSlide++;
+  }, TIMEOUT);
 
   mainWindow.loadURL(electronConfig.DASHBOARD_ONE);
   console.log("Dashboard Loaded Successfully");
