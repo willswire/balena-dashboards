08e06438377e574581beba33188761e486289439
Will Walker <willwalker@facil-7f1jg12.facilities.udel.edu>
2019-01-23 10:33:18 -0500

    password

--- dashboard/app/start.sh
+++ dashboard/app/start.sh
@@ -6,6 +6,10 @@ umount /dev/shm && mount -t tmpfs shm /dev/shm
 # Remove any temp data
 rm /tmp/.X0-lock &>/dev/null || true
 
+# Set the X11VNC Password
+mkdir ~/.x11vnc
+x11vnc -storepasswd $NOVNC_PASSWORD ~/.x11vnc/passwd
+
 # Start the UI pointing to ElectronJS
 startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app &
-x11vnc -quiet -noxrecord -noxfixes -noxdamage -forever -localhost -passwd $NO_VNCPASSWORD
+x11vnc -quiet -noxrecord -noxfixes -noxdamage -forever -localhost -rfbauth ~/.x11vnc/passwd
