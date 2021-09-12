2e24f07b66c9995aac6dad3754fa533daa6cfa1f
epicdistraction <daniel@epicdistraction.com>
2021-09-12 07:45:39 -0700

    Update start.sh to latest novnc script name (#17)
    The noVNC project has updated the command to invoke the application. It went from /utils/launch.sh to /utils/novnc_proxy per this commit: https://github.com/novnc/noVNC/commit/89e206c14601712cc713ce6f64e2f27f292bc3c5
    
    I've also added a remove command to the /noVNC/index.html file; since, that step sends a warning on subsequent builds.

--- vnc/start.sh
+++ vnc/start.sh
@@ -5,7 +5,8 @@ iptables -A INPUT -p tcp --dport 80 -i resin-vpn -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j REJECT
 
 # Rename the files to allow for default connection html page
+rm /noVNC/index.html
 ln -s /noVNC/vnc.html /noVNC/index.html
 
 # Start noVNC
-/noVNC/utils/launch.sh --vnc 127.0.0.1:5900 --listen 80
+/noVNC/utils/novnc_proxy --vnc 127.0.0.1:5900 --listen 80
